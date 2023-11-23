048ebc97295a9866add6b3bb0431cf20c4ed5c1d
Mike-Mandra <michael.mandra@dcmail.ca>
2023-11-23 12:11:52 -0500

    Loop through array
    Looped through the array of filenames, created a constant called imgELement used the src and altText of previous arrays. appended thumbbar to each img element

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -7,6 +7,8 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the array of image filenames */
 const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
 
+
+/* Declaring the alternative text for each image file */
 const altText = {
     "pic1.jpg": "Blue eye",
     "pic2.jpg": "Rock that looks like a wave",
@@ -15,11 +17,14 @@ const altText = {
     "pic5.jpg": "Monarch butterfly"
 }
 
-
-
-/* Declaring the alternative text for each image file */
-
 /* Looping through images */
+for (counter = 0; counter < images.length; counter++) 
+{
+    const imgElement = document.createElement("img");
+    imgElement.src = images[counter];
+    imgElement.alt = altText[images[counter]]
+    thumbBar.appendChild(imgElement);
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
