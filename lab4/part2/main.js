1fdf0cd6bec4ee52bb646ab9b2c42d38617a466e
Mike-Mandra <michael.mandra@dcmail.ca>
2023-11-23 11:55:52 -0500

    Const object
    Declared a constant object altText and added alt  text for each image

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -5,6 +5,17 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
+
+const altText = {
+    "pic1.jpg": "Blue eye",
+    "pic2.jpg": "Rock that looks like a wave",
+    "pic3.jpg": "Purple and white flowers",
+    "pic4.jpg": "Egyptian pharohs",
+    "pic5.jpg": "Monarch butterfly"
+}
+
+
 
 /* Declaring the alternative text for each image file */
 
