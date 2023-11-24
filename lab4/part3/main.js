cc23e2f2d741944ee679706f0fd9cbcc651c3fc2
Mike-Mandra <michael.mandra@dcmail.ca>
2023-11-24 12:34:23 -0500

    Repositioned code, created new ball
    Repositioned the Ball class and draw method(), Also added the const testball and made it appear on the screen

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -6,19 +6,6 @@ const ctx = canvas.getContext('2d');
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// function to generate random number
-
-function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num;
-}
-
-// function to generate random color
-
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
-
 class Ball {
   constructor(x, y, velX, velY, color, size) {
     this.x = x;
@@ -27,7 +14,6 @@ class Ball {
     this.velY = velY;
     this.color = color;
     this.size = size;
-    
   }
 
   draw() {
@@ -35,6 +21,21 @@ class Ball {
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
-    
   }
 }
+
+const testBall = new Ball(50, 100, 4, 4, "blue", 10);
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+console.log(testBall.x);
+console.log(testBall.size);
+console.log(testBall.color);
+testBall.draw();
