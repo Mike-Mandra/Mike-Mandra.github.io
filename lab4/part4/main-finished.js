fac17915bed0dd46dbcbf7f262cf0b68b2a097d6
Mike-Mandra <michael.mandra@dcmail.ca>
2023-11-24 13:48:51 -0500

    Updated  ball Class
    Added extends shape to ball class, used super to call shape constructor

--- lab4/part4/main-finished.js
+++ lab4/part4/main-finished.js
@@ -28,13 +28,10 @@ class Shape {
    }
 }
 
-class Ball {
+class Ball extends Shape {
 
    constructor(x, y, velX, velY, color, size) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
+      super(x, y, z, velX, velY)
       this.color = color;
       this.size = size;
    }
